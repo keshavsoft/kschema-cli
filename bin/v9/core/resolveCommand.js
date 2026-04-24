ec26b9f415dfe293687451ea1a1445b760162085
keshavsoft <nknnkumar@yahoo.com>
2026-04-24 18:41:59 +0530

    v 1.9.3 cli help emojis

--- bin/v9/core/resolveCommand.js
+++ bin/v9/core/resolveCommand.js
@@ -2,10 +2,11 @@ import init from "../commands/init.js";
 import test from "../commands/test.js";
 import generateSamples from "../commands/generateSamples.js";
 
+// resolveCommand.js
 const map = {
     init,
     test,
-    generateSamples
+    "generate-samples": generateSamples
 };
 
 export default function resolveCommand(cmd) {
