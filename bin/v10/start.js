dd9bf4c4a7b0aea494af92121b816e02820b58e4
keshavsoft <nknnkumar@yahoo.com>
2026-04-27 07:11:12 +0530

    version pefect shown in help of showusage

--- bin/v10/start.js
+++ bin/v10/start.js
@@ -1,14 +1,20 @@
+import parseInput from "./core/parseInput.js";
+import resolveCommand from "./core/resolveCommand.js";
 import showUsage from './core/showUsage.js';
 import pkg from '../../package.json' with { type: 'json' };
 
 const version = pkg.version;
 
 const run = async () => {
-  if (!command) {
-    console.log(`Unknown command: ${input.cmd}\n`);
+  const input = parseInput();
 
-    return showUsage(version);
-  }
+  if (!input.cmd) return showUsage(version);
+
+  if (input.cmd === "--help" || input.cmd === "-h" || input.cmd === "help") return showUsage(version);
+
+  const command = resolveCommand(input.cmd);
+
+  if (!command) return (console.log(`Unknown command: ${input.cmd}\n`), showUsage(version));
 
   await command(input);
 };
