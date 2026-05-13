984bb0962b442535f3fee102a8744c6d03b86f4a
keshavsoft <nknnkumar@yahoo.com>
2026-05-13 10:16:38 +0530

    v 1.15.1

---
+++ bin/v15/commands/tally/template/v1/Public/Tally/Masters/V6/Ledgers/Index/FormLoad/DomContentLoaded/AddListeners/StockItemsHtmlId/clickFunc.js
@@ -0,0 +1,96 @@
+import { initTableOnly } from "../../../../../../../../../KSTableAi/V26/entry.js";
+
+const clickFuncToRun = () => {
+    buildUi().then();
+};
+
+const buildUi = async () => {
+    const config = {
+        containerId: 'kSTableContainer',
+        tableName: "StockItems-",
+        mode: "create", // or "show" "create"
+        layout: {
+            verticalPosition: "top", // or left/right/bottom
+            type: "table", // "vertical" | "table" | "vertical-table"
+        },
+        endPoints: {
+            create: "/Api/V9/ItemsTable/Insert",
+            update: "",
+            read: "/Api/V1/StockItems/ShowAll",
+            find: "/Api/V9/BillsTable/find"
+        },
+        options: {
+            firstRow: {
+                showSearch: true
+            },
+            vertical: {
+                showVertical: true,
+                isDisabled: false,
+                showSaveButton: true
+            },
+            dataList: {
+                show: true,
+            },
+            table: {
+                isDisabled: false,
+                showTable: true,
+                showRowOptions: false,
+                showSerial: true,
+                showFooter: false,
+                footer: {
+                    showDataList: true
+                }
+            },
+            focus: {
+                priority: ["vertical", "footer", "search"]
+            }
+        },
+        columnsConfig: [
+            {
+                columnName: "StockItemName"
+            },
+            {
+                columnName: "StockItemReservedName"
+            },
+            {
+                columnName: "StockItemType"
+            },
+            {
+                columnName: "StockBaseUnits"
+            },
+            {
+                columnName: "sgstRate"
+            },
+            {
+                columnName: "cgstRate"
+            }
+        ],
+        uiClasses: {
+            form: {
+                formClass: 'grid grid-cols-3 gap-x-8 gap-y-4 p-6 verticalForm',
+                buttonClass: 'mt-2 px-4 py-1 bg-green-500 text-white',
+                rowClass: 'flex items-center space-x-4',
+                labelClass: 'w-24 text-sm font-medium',
+                inputClass: 'flex-1 border rounded px-3 py-2'
+            }
+        },
+        callbacks: {
+            table: {
+                onDelete: async ({ toDeletePk }) => {
+                    const fromDelete = await startFetchAsGet({
+                        inQuery: {
+                            ParentPk: toDeletePk
+                        }
+                    });
+
+                    return await fromDelete;
+                }
+            }
+        }
+    };
+
+    await initTableOnly(config);
+    // window.KSTable.initTableOnly(config);
+};
+
+export { clickFuncToRun };
